d874b115e9484e5544c2a93b76aaeab6073da67f
connor <vectrixu@gmail.com>
2017-11-02 10:38:38 +1300

    Make observer action blueprint no longer abstract

--- src/main/java/com/abilityapi/sequenceapi/action/type/observe/ObserverActionBlueprint.java
+++ src/main/java/com/abilityapi/sequenceapi/action/type/observe/ObserverActionBlueprint.java
@@ -1,6 +1,7 @@
 package com.abilityapi.sequenceapi.action.type.observe;
 
-public abstract class ObserverActionBlueprint<T> {
+public
+class ObserverActionBlueprint<T> {
 
     public ObserverAction<T> create() {
         return new ObserverAction<>();
