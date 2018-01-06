5e5b8f1fb132b715af1a364c712d67847dc60497
Connor Hartley <vectrixu+gh@gmail.com>
2018-01-07 11:19:07 +1300

    Make more clearly defined unique sequence holders

---
+++ src/main/java/com/abilityapi/sequenceapi/SequenceHolder.java
@@ -0,0 +1,8 @@
+package com.abilityapi.sequenceapi;
+
+/**
+ * An interface that represents a class
+ * that holds a sequence.
+ */
+public interface SequenceHolder {
+}
--- src/main/java/com/abilityapi/sequenceapi/SequenceRegistry.java
+++ src/main/java/com/abilityapi/sequenceapi/SequenceRegistry.java
@@ -14,20 +14,21 @@ import java.util.Set;
  */
 public class SequenceRegistry<T> implements Iterable<SequenceBlueprint<T>> {
 
-    private final HashMap<Class<?>, SequenceBlueprint<T>> registry = Maps.newHashMap();
+    private final HashMap<Class<? extends SequenceHolder>, SequenceBlueprint<T>> registry = Maps.newHashMap();
 
     /**
      * Puts the {@link SequenceBlueprint} into this registry.
      *
      * <p>The {@link SequenceContext} must contain a unique
-     * {@link SequenceContext#OWNER} to provide ownership over
-     * a {@link Sequence}.</p>
+     * {@link SequenceContext#ROOT} to provide ownership over
+     * a {@link Sequence} which is an instance of a class that
+     * implements {@link SequenceHolder}.</p>
      *
      * @param sequenceBlueprint the sequence blueprint
      * @return true if the blueprint was added, false if it was not
      */
     public final boolean put(final SequenceBlueprint<T> sequenceBlueprint) {
-        this.registry.put(sequenceBlueprint.getContext().getOwner(), sequenceBlueprint);
+        this.registry.put(sequenceBlueprint.getContext().<SequenceHolder>getRoot().getClass(), sequenceBlueprint);
 
         return true;
     }
@@ -39,7 +40,7 @@ public class SequenceRegistry<T> implements Iterable<SequenceBlueprint<T>> {
      * @param key the key
      * @return the sequence blueprint
      */
-    public final <O> Optional<SequenceBlueprint<T>> get(final Class<? extends O> key) {
+    public final Optional<SequenceBlueprint<T>> get(final Class<? extends SequenceHolder> key) {
         return Optional.ofNullable(this.registry.get(key));
     }
 
@@ -48,7 +49,7 @@ public class SequenceRegistry<T> implements Iterable<SequenceBlueprint<T>> {
      *
      * @return the sequence key, or {@code null} if they check is not contained in this registry
      */
-    public final Set<Class<?>> keySet() {
+    public final Set<Class<? extends SequenceHolder>> keySet() {
         return this.registry.keySet();
     }
 
